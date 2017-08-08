8bae8093b9d1104922ebfa703529b0f62c3a8d59
sabbirsr <rakibhasansabbir@yahoo.com>
2017-08-08 07:39:13 +0600

    create search option

--- app/src/main/java/bd/ac/seu/my/mymaps/GMActivity.java
+++ app/src/main/java/bd/ac/seu/my/mymaps/GMActivity.java
@@ -1,12 +1,18 @@
 package bd.ac.seu.my.mymaps;
 
 import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.identity.intents.Address;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,36 +20,40 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.IOException;
+import java.util.List;
+
 public class GMActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap mGoogleMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(googleServicesAvailable()){
-            Toast.makeText(this,"wow....", Toast.LENGTH_LONG).show();
+        if (googleServicesAvailable()) {
+            Toast.makeText(this, "wow....", Toast.LENGTH_LONG).show();
             setContentView(R.layout.activity_gm);
             initMap();
-        }else {
+        } else {
             //No google map layout
         }
     }
 
     private void initMap() {
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fragment);
         mapFragment.getMapAsync(this);
     }
 
-    public boolean googleServicesAvailable(){
+    public boolean googleServicesAvailable() {
         GoogleApiAvailability api = GoogleApiAvailability.getInstance();
         int isAvailable = api.isGooglePlayServicesAvailable(this);
-        if(isAvailable == ConnectionResult.SUCCESS){
+        if (isAvailable == ConnectionResult.SUCCESS) {
             return true;
-        }else if(api.isUserResolvableError(isAvailable)){
+        } else if (api.isUserResolvableError(isAvailable)) {
             Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
             dialog.show();
-        }else {
-            Toast.makeText(this,"Cant Connect to play services", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Cant Connect to play services", Toast.LENGTH_LONG).show();
 
         }
         return false;
@@ -52,7 +62,19 @@ public class GMActivity extends AppCompatActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
-        goToLocationZoom(24.827876,89.3514836,15);
+        //goToLocationZoom(24.827876,89.3514836,20);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mGoogleMap.setMyLocationEnabled(true);
     }
 
     private void goToLocation(double lat, double log) {
@@ -64,5 +86,23 @@ public class GMActivity extends AppCompatActivity implements OnMapReadyCallback 
         LatLng ll = new LatLng(lat, log);
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll,zoom);
         mGoogleMap.moveCamera(update);
+    }
+
+    public void goLocation(View view) throws IOException {
+        EditText et = (EditText) findViewById(R.id.editText);
+        String location = et.getText().toString();
+
+        //Takes string and convert it latitude and longitude
+        Geocoder gc = new Geocoder(this);
+        List<android.location.Address> list = gc.getFromLocationName(location, 1);
+        android.location.Address address = list.get(0);
+        String locality = address.getLocality();
+
+        Toast.makeText(this,"Now you are in "+ locality, Toast.LENGTH_LONG).show();
+
+        double lat = address.getLatitude();
+        double log = address.getLongitude();
+        goToLocationZoom(lat,log,15);
+
     }
 }
